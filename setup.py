738d3600b2223b41c97469a173de2240221c8a4c
Andy Lu <andy@stitchdata.com>
2019-09-19 19:54:10 +0000

    Bump to v0.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="tap-iterable",
-    version="0.0.2",
+    version="0.0.3",
     description="Singer.io tap for extracting Iterable data",
     author="Stitch",
     url="http://github.com/singer-io/tap-iterable",
